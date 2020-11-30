6886bb6ff96617c8dfc360cd890066619a038dfa
Anan <anan.mzean@gmail.com>
2020-12-01 00:18:54 +0700

    first commit

---
+++ apps/secue-web/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './index.module.less';
+import Layout from '../components/layout';
+
+export const Index = () => {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+};
+
+export default Index;
