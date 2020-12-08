dc4ad52723823cc53df11eb8b7341b170b00b185
Anan <anan.mzean@gmail.com>
2020-12-09 01:57:36 +0700

    ver1

---
+++ apps/secue-web/components/navbar/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './navbar.less';
+export const Index = () => {
+  return (
+    <div className={styles.centerContainer}>
+      <div className={styles.MainContainer}>
+        <div className={styles.flexContainer}>
+        <Image
+          className={styles.leftLogo}
+          src="/images/logo1.png"
+          alt="secue-logo2"
+          width={68}
+          height={68}
+        />
+        <div className={styles.TitleHome}>Home</div>
+        <div className={styles.TitleWork}>Our Works</div>
+        <div className={styles.TitleContact}>Contact Us</div>
+        <div className={styles.TitleAbout}>About Us</div>
+        <Image
+          className={styles.rightLogo}
+          src="/images/logo2.png"
+          alt="secue-logo2"
+          width={68}
+          height={68}
+        />
+        </div>
+      </div>
+      </div>
+  );
+};
+
+export default Index;
--- apps/secue-web/pages/index.tsx
+++ apps/secue-web/pages/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './index.module.less';
 import Layout from '../components/layout';
-
+import NavBar from '../components/navbar';
+import AbsoluteImage from '../components/absoluteImage';
+import AbsoluteImage2 from '../components/absoluteImage2';
+import '../public/styles/global.less';
 export const Index = () => {
   return (
     <>
-      <Layout />
+    <NavBar />
+    <div className={styles.MainContainer}>
+      
+      <AbsoluteImage />
+      <AbsoluteImage2 />
+    </div>
     </>
   );
 };
