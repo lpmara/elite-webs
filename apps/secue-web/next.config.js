bf2a9be2cbe457fdf133eadad7e3e7dff2272f55
Anan <anan.mzean@gmail.com>
2021-01-04 02:44:49 +0700

    xxx

--- apps/secue-web/next.config.js
+++ apps/secue-web/next.config.js
@@ -1,10 +1,23 @@
 const withNx = require('@nrwl/next/plugins/with-nx');
 
 const withLess = require('@zeit/next-less');
+// module.exports = withLess(
+//   withNx({
+//     cssModules: true,
+//   })
+// );
+
 module.exports = withLess(
   withNx({
-    // Set this to true if you use CSS modules.
-    // See: https://github.com/css-modules/css-modules
     cssModules: true,
+    webpack: (config) => {
+      config.plugins.map((value) => {
+        if (value.options) {
+          value.options.ignoreOrder = true;
+        }
+      });
+
+      return config;
+    },
   })
 );
